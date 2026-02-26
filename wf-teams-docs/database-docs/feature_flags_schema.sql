cea68e9669e3709a105850e5f8c05f15010846de
wfnfwinitiative <wf.nfw.initiative@gmail.com>
2026-02-26 09:57:43 +0530

    Update feature_flags_schema.sql

--- wf-teams-docs/database-docs/feature_flags_schema.sql
+++ wf-teams-docs/database-docs/feature_flags_schema.sql
@@ -1,1 +1,12 @@
+CREATE TABLE wfnfw.feature_flags (
+    id BIGSERIAL PRIMARY KEY,
+    feature_flag_name VARCHAR(100) NOT NULL UNIQUE,
+    enabled BOOLEAN DEFAULT false NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
 
+CREATE INDEX idx_feature_flag_name ON wfnfw.feature_flags(feature_flag_name);
+
+
+INSERT INTO wfnfw.feature_flags (feature_flag_name, enabled) VALUES ('voice_llm_service', false);
